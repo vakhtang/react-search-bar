35960e82dd9fcd4547acfd2e3883a29023c78dec
vakhtang <vakhtang@s3.io>
2015-08-17 19:36:06 -0400

    Rename demo entry file

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: ['webpack/hot/dev-server', './app.js']
+    app: ['webpack/hot/dev-server', './demo.jsx']
   },
   module: {
     loaders: [
